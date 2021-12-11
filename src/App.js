3949f47ca5a476af343300a83a13c370c101f0ac
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-12-11 18:04:18 +0100

    added the cat page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import Homepage from '../src/Pages/Homepage/Homepage.component';
+import { Switch, Route} from 'react-router-dom'
+
+
+const HatPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-     <Homepage/>
+      <Switch>
+        <Route exact path="/" component={Homepage}/>
+        <Route path="/hats" component={HatPage} />
+      </Switch>
     </div>
   );
 }
