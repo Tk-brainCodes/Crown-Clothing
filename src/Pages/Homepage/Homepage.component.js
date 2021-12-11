291eec7e87ff9a4477f2f0a616e1b4eb40ce1a6c
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-12-11 15:48:06 +0100

    Pages folder components

---
+++ src/Pages/Homepage/Homepage.component.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Directory from '../../Components/directory/directory.component';
+import './Homepage.style.scss';
+
+const Homepage = () => {
+    return (
+        <div className="Homepage">
+          <Directory/>
+        </div>
+    )
+}
+
+export default Homepage;
