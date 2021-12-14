4f4563b83bb1a4d8b4d24f54ef13bf2f1c59f974
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-12-14 14:05:10 +0100

    Added collection component

--- src/Components/collection-item/collection-item.component.jsx
+++ src/Components/collection-item/collection-item.component.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './collection-item.styles.scss';
 
-const CollectionItem = () => {
+const CollectionItem = ({id, price, name, imageUrl}) => {
     return (
-        <div>
-            
+        <div className='collection-item'>
+            <div 
+            className='image'
+            style={{
+             backgroundImage: `url(${imageUrl})`
+
+            }}
+            />
+            <div className="collection-footer">
+                <span className="name">{name}</span>
+                <span className="price">${price}</span>
+            </div>
         </div>
     )
 }
