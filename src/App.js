afe554ea39c07bef72fe8ef7fcac2f564e5962cb
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-12-15 13:16:31 +0100

    added sign in route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import Homepage from '../src/Pages/Homepage/Homepage.component';
 import Shop from './Pages/Shop/shop.component';
+import Header from './Components/header/header.component';
+import SignInAndSignUpPage from './Pages/sign-in-and-sign-up/sign-in-and-sign-up.components';
 import { Switch, Route} from 'react-router-dom'
 
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Switch>
         <Route exact path="/" component={Homepage}/>
         <Route path="/Shop" component={Shop} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
