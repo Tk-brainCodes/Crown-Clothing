884c64444f377000840b6966f26b71258fd8b6cc
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-12-15 13:19:15 +0100

    sign and sign up page

---
+++ src/Pages/sign-in-and-sign-up/sign-in-and-sign-up.components.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './sign-in-and-sign-up.styles.scss';
+import SignIn from '../../Components/sign-in/sign.in.component';
+
+const SignInAndSignUpPage = () => {
+    return (
+        <div className='sign-in-and-sign-up'>
+           <SignIn/>
+        </div>
+    )
+}
+
+export default SignInAndSignUpPage;
