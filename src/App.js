0887949d31d34e69daef5c0cdd7b45f0e7c5742c
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-12-16 14:19:44 +0100

    Added current user state and enabled user authentication from firebase

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Homepage from '../src/Pages/Homepage/Homepage.component';
 import Shop from './Pages/Shop/shop.component';
 import Header from './Components/header/header.component';
 import SignInAndSignUpPage from './Pages/sign-in-and-sign-up/sign-in-and-sign-up.components';
 import { Switch, Route} from 'react-router-dom'
-
+import {auth} from './firebase/firebase.utils';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  let unSubscribeFromAuth = null;
+  
+  useEffect(() => {
+   unSubscribeFromAuth = auth.onAuthStateChanged(user => {
+      setCurrentUser(user);
+      return () => {
+        unSubscribeFromAuth();
+      }
+    }, [currentUser]);
+  });
+
+  
   return (
     <div className="App">
-      <Header/>
+      <Header currentUser={currentUser}/>
       <Switch>
         <Route exact path="/" component={Homepage}/>
         <Route path="/Shop" component={Shop} />
