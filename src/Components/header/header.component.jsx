c86398ab98efc586a95ff8e73868d0d67ed4fe61
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-12-16 14:23:19 +0100

    added conditional rendering of sign out button and sign in button

--- src/Components/header/header.component.jsx
+++ src/Components/header/header.component.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { auth } from '../../firebase/firebase.utils';
 import {ReactComponent as Logo} from '../../assets/crown.svg'
 import './header.styles.scss';
-const Header = () => {
+const Header = ({currentUser}) => {
     return (
         <div className='header'>
             <Link className='logo-container' to="/">
@@ -11,7 +12,16 @@ const Header = () => {
             <div className='options'>
                 <Link className="option" to="/shop">SHOP</Link>
                  <Link className="option" to="/contact">CONTACT</Link>
-            </div>
+                 {
+                     currentUser ?
+                     (
+                     <div className='option' onClick={() => auth.signOut()}>
+                        SIGN OUT
+                     </div>
+                     ) :
+                     <Link className='option' to="/signin">SIGN IN</Link>
+                 }
+            </div> 
         </div>
     )
 }
