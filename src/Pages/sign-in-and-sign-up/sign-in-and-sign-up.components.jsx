e37f06df1c4c3852da0e5de2d6440d25afc672ad
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-12-20 00:19:35 +0100

    Added sign up component to page

--- src/Pages/sign-in-and-sign-up/sign-in-and-sign-up.components.jsx
+++ src/Pages/sign-in-and-sign-up/sign-in-and-sign-up.components.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './sign-in-and-sign-up.styles.scss';
 import SignIn from '../../Components/sign-in/sign.in.component';
+import SignUp from '../../Components/sign-up/sign-up.components';
 
 const SignInAndSignUpPage = () => {
     return (
         <div className='sign-in-and-sign-up'>
            <SignIn/>
+           <SignUp/>
         </div>
     )
 }
