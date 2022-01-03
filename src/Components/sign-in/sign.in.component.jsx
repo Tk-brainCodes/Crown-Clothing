d186f86be4b038568c9e20af9825c989a79ec7f8
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-01-03 17:40:28 +0100

    modified

--- src/Components/sign-in/sign.in.component.jsx
+++ src/Components/sign-in/sign.in.component.jsx
@@ -1,17 +1,23 @@
 import React, {useState} from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-buttons/custom-buttons.component';
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils';
 import './sign.in.styles.scss';
 
 const SignIn = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async e => {
          e.preventDefault();
-         setEmail('');
-         setPassword('');
+    try {
+        await auth.signInWithEmailAndPassword(email, password)
+        setEmail('')
+        setPassword('')
+
+    } catch (error) {
+        console.log(error);
+    }
     }
 
     const handleChangeEmail = (e) => {
