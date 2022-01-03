ad1bccbfebf91416710821e664b3339eb7bc0999
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-01-03 17:43:40 +0100

    added user types

---
+++ src/redux/root-reducer.js
@@ -0,0 +1,8 @@
+//the root reducer represents all the reducers based on what has been created
+
+import {combineReducers} from 'redux';
+import userReducer from './user/user.reducer';
+
+export default  combineReducers({
+    user: userReducer
+});
