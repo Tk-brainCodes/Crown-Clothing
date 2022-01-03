88eb031023aafdd2cca746a351c7735e4386454d
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-01-03 17:37:31 +0100

    Added mapStateToProps and redux connect

--- src/Components/header/header.component.jsx
+++ src/Components/header/header.component.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {connect} from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
 import {ReactComponent as Logo} from '../../assets/crown.svg'
 import './header.styles.scss';
+
+
 const Header = ({currentUser}) => {
+    console.log(currentUser, "the current user");
     return (
         <div className='header'>
             <Link className='logo-container' to="/">
@@ -23,7 +27,9 @@ const Header = ({currentUser}) => {
                  }
             </div> 
         </div>
-    )
+    );
 }
-
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+export default connect(mapStateToProps)(Header);
