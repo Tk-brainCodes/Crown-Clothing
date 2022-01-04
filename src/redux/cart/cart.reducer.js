a0c194c6f8d452b9b62f88b6f9314851e3fbb3dc
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-01-04 11:42:19 +0100

    updated state and action

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,4 @@
-import { UserActionTypes } from "./cart.action.types"
+import { CartActionTypes } from "./cart.action.types"
 
 const INITIAL_STATE = {
     hidden: true
@@ -6,7 +6,7 @@ const INITIAL_STATE = {
 
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-      case UserActionTypes.TOGGLE_CART_HIDDEN:
+      case CartActionTypes.TOGGLE_CART_HIDDEN:
           return {
               ...state,
               hidden: !state.hidden
