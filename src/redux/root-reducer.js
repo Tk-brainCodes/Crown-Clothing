9bee0bf14fafa16b91b7f39bc7c34b4b478bc972
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-01-12 16:20:38 +0100

    added state local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,21 @@
 //the root reducer represents all the reducers based on what has been created
 
 import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
 import userReducer from './user/user.reducer'
 import cartReducer from './cart/cart.reducer'
+import storage from 'redux-persist/lib/storage'
 
-export default combineReducers({
+const persistConfig = {
+key: 'root',
+storage,
+whitelist: ['cart'] //what we want to persist 
+}
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 })
+
+
+export default persistReducer(persistConfig, rootReducer);
