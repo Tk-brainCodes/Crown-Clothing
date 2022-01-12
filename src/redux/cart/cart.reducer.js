42304be2321e7d1624d25729a23b086b9260ec68
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-01-12 16:18:26 +0100

    added remove item state

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import { CartActionTypes } from "./cart.action.types";
-import { addItemToCart } from "./cart.utils";
+import { addItemToCart, removeItemsFromCart } from "./cart.utils";
 
 const INITIAL_STATE = {
     hidden: true,
@@ -18,6 +18,16 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                   ...state,
                   cartItem: addItemToCart(state.cartItem, action.payload)
               }
+          case CartActionTypes.CLEAR_ITEM_FROM_CART:
+              return {
+                  ...state,
+                  cartItem: state.cartItem.filter(cartItems => cartItems.id !== action.payload.id)
+              }
+          case CartActionTypes.REMOVE_ITEM:
+              return {
+                  ...state,
+                  cartItem: removeItemsFromCart(state.cartItem, action.payload)
+              }        
       default:
           return state
   }
