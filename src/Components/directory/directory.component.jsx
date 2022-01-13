b271518828ce0e665646d7862a8c4370f5e2cddd
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-01-13 10:57:59 +0100

    added mapSateToProps

--- src/Components/directory/directory.component.jsx
+++ src/Components/directory/directory.component.jsx
@@ -1,44 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
+import {connect} from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 import MenuItem from '../menu-item/menu-item.components';
 import './directory.styles.scss';
 
-function Directory() {
-    const [sections] = useState(
-        [
-            {
-                title: 'hats',
-                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                id: 1,
-                linkUrl: "hats"
-            },
-            {
-                title: 'jackets',
-                imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                id: 2,
-                linkUrl: ''
-            },
-            {
-                title: 'sneakers',
-                imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                id: 3,
-                linkUrl: ""
-            },
-            {
-                title: 'womens',
-                imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                size: 'large',
-                id: 4,
-                linkUrl: ""
-            },
-            {
-                title: 'mens',
-                imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                size: 'large',
-                id: 5,
-                linkUrl: ""
-            }
-        ]
-    );
+function Directory({sections}) {
     return (
         <div className='directory-menu'>
            {sections.map(({id, ...otherSectionProps}) => (
@@ -48,4 +15,7 @@ function Directory() {
     );
 }
 
-export default Directory;
+const mapStateToProps = createStructuredSelector({
+    sections: selectDirectorySections
+})
+export default connect(mapStateToProps)(Directory);
