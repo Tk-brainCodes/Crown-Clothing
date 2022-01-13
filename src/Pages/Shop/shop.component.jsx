37eafeb552e6b8023e7aba37404fdf872db78ad9
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-01-13 11:23:16 +0100

    added mapStateToProps

--- src/Pages/Shop/shop.component.jsx
+++ src/Pages/Shop/shop.component.jsx
@@ -1,9 +1,11 @@
-import React, {useState} from 'react';
-import SHOP_DATA from './shop.data';
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectShopItems } from '../../redux/shop/shop.selector';
 import PreviewCollection from '../../Components/preview-collection/preview-collection.component';
 
-const Shop = () => {
-    const [collections] = useState(SHOP_DATA);
+const Shop = ({collections}) => {
+
     return (
         <div className="shop-page">
             {collections.map(({id, ...otherCollectionProps}) => (
@@ -12,4 +14,8 @@ const Shop = () => {
         </div>)
 }
 
-export default Shop;
+const mapStateToProps = createStructuredSelector({
+    collections: selectShopItems
+})
+
+export default connect(mapStateToProps)(Shop);
