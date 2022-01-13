62fb79e32d80ec3fd4b22de6e062c97a7ba1ab39
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-01-13 10:58:37 +0100

    added directory to root reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,6 +4,7 @@ import { combineReducers } from 'redux'
 import { persistReducer } from 'redux-persist'
 import userReducer from './user/user.reducer'
 import cartReducer from './cart/cart.reducer'
+import directoryReducer from './directory/directory.reducer'
 import storage from 'redux-persist/lib/storage'
 
 const persistConfig = {
@@ -14,7 +15,8 @@ whitelist: ['cart'] //what we want to persist
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer
 })
 
 
