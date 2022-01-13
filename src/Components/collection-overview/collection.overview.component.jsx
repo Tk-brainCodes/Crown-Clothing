fb90e345541f84ecff4dec84b3bbd3c286d97c98
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-01-13 15:12:10 +0100

    collection preview component

---
+++ src/Components/collection-overview/collection.overview.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+import { selectShopItems } from '../../redux/shop/shop.selector'
+import PreviewCollection from '../preview-collection/preview-collection.component'
+import './collection.overview.styles.scss'
+const CollectionOverview = ({collections}) => {
+    return (
+        <div className='collection-overview'>
+            {
+  collections.map(({ id, ...otherCollectionProps }) => (
+    <PreviewCollection key={id} {...otherCollectionProps} />
+  ))
+}
+        </div>
+    )
+}
+
+const mapStateToProps = createStructuredSelector({
+    collections: selectShopItems
+})
+
+export default connect(mapStateToProps)(CollectionOverview);
