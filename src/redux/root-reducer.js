7ac90c1d87f089f9fc553c156d98a6c33b75cfc2
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-01-13 11:24:50 +0100

    added shop to root reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,6 +5,7 @@ import { persistReducer } from 'redux-persist'
 import userReducer from './user/user.reducer'
 import cartReducer from './cart/cart.reducer'
 import directoryReducer from './directory/directory.reducer'
+import shopReducer from './shop/shop.reducer'
 import storage from 'redux-persist/lib/storage'
 
 const persistConfig = {
@@ -16,7 +17,8 @@ whitelist: ['cart'] //what we want to persist
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
-  directory: directoryReducer
+  directory: directoryReducer,
+  shop: shopReducer
 })
 
 
