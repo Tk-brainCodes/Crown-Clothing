7f4994dbb9a625434bf6c0180b1f07b7a3232152
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-01-15 10:39:51 +0100

    added selector to map through object

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,8 +1,19 @@
 import { createSelector } from 'reselect'
 
+
 const selectShop = state => state.shop
 
-export const selectShopItems = createSelector(
+export const selectCollections = createSelector(
   [selectShop],
   shop => shop.collections
+)
+
+export const selectCollectionForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key]) //map the keys of the collections to an array
+)
+
+export const selectCollection = collectionUrlParams => createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParams]
 )
