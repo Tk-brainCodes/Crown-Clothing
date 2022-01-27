e0229b5b627d81c0eee987493d43ae64e0c70c71
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-01-28 00:00:28 +0100

    chore: added new components

--- src/Components/header/header.component.jsx
+++ src/Components/header/header.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import {connect} from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
+import { HeaderStyledContainer, LogoContainer, OptionDiv,OptionLink,OptionsContainer } from './header.styles';
 import {ReactComponent as Logo} from '../../assets/crown.svg'
 import CartIcon from '../cart-icons/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
@@ -10,26 +10,26 @@ import './header.styles.scss';
 
 const Header = ({currentUser, hidden}) => {
     return (
-        <div className='header'>
-            <Link className='logo-container' to="/">
+        <HeaderStyledContainer>
+            <LogoContainer to="/">
               <Logo className="logo"/>
-            </Link>
-            <div className='options'>
-                <Link className="option" to="/shop">SHOP</Link>
-                 <Link className="option" to="/contact">CONTACT</Link>
+            </LogoContainer>
+            <OptionsContainer>
+                <OptionLink to="/shop">SHOP</OptionLink>
+                 <OptionLink to="/contact">CONTACT</OptionLink>
                  {
                      currentUser ?
                      (
-                     <div className='option' onClick={() => auth.signOut()}>
+                     <OptionDiv onClick={() => auth.signOut()}>
                         SIGN OUT
-                     </div>
+                     </OptionDiv>
                      ) :
-                     <Link className='option' to="/signin">SIGN IN</Link>
+                     <OptionLink to="/signin">SIGN IN</OptionLink>
                  }
                  <CartIcon/>            
-                 </div> 
+                 </OptionsContainer> 
                   {hidden ? null : <CartDropdown />}
-        </div>
+        </HeaderStyledContainer>
     );
 }
 const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
