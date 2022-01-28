b9db4161c1c7dca494c35a080b96b4ca1bae4504
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-01-28 11:45:41 +0100

    chore: Updated Component for div

--- src/Components/custom-buttons/custom-buttons.component.jsx
+++ src/Components/custom-buttons/custom-buttons.component.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
-import './custom-buttons.styles.scss';
-const CustomButton = ({children,isGoogleSignIn,inverted, ...otherProps}) => {
-    return (
-     <button className={
-       `${inverted ? 'inverted' : ''}
-       ${isGoogleSignIn ? 'google-sign-in' : ''}
-       custom-button`} 
-       {...otherProps}
-       >
-         {children}
-     </button>
-    )
+import { CustomButtonContainer } from './custom-buttons.styles'
+const CustomButton = ({children, ...props}) => {
+  return (
+    <CustomButtonContainer {...props}>
+      {children}
+    </CustomButtonContainer>
+  )
 }
 
-export default CustomButton;
+export default CustomButton
