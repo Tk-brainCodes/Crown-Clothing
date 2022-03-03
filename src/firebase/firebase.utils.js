8f3ee61190a7325138c888ef5710e2f5bc35da75
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-03-03 13:31:52 +0100

    added convertCollectionSnapshotToMap

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -20,6 +20,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   if(!userAuth) return;
   const userRef = firestore.doc(`users/${userAuth.uid}`);
   const snapShot = await userRef.get();
+
   if(!snapShot.exists) {
     const {displayName, email} = userAuth;
     const createdAt = new Date();
@@ -35,8 +36,35 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   }
  }
   return userRef;
+} 
+
+export const addCollectionAndDocuments = async (collectionKey, objectToAdd) => {
+   const collectionRef = firestore.collection(collectionKey);
+   const batch = firestore.batch();
+   objectToAdd.forEach(obj => {
+     const newDocRef = collectionRef.doc();
+     batch.set(newDocRef, obj);
+   });   
+  return await batch.commit() //fireoff batch request
 }
 
+ export const convertCollectionSnapshotToMap = collections => {
+   const transformCollection = collections.docs.map(doc => {
+     const {title, items} = doc.data();
+
+     return {
+       routeName: encodeURI(title.toLowerCase()),
+       id: doc.id,
+       title,
+       items
+     };
+   });
+   return transformCollection.reduce((accumulator, collection) => {
+     accumulator[collection.title.toLowerCase()] = collection;
+     return accumulator;
+   }, {})
+ }
+ 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({prompt: 'select_account'}); //always trigger a google popup for auth and signin
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
