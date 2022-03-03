d29e670e35b2bd63b8dfbb929b97f1885b750e64
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-03-03 13:28:54 +0100

    removed types

--- src/Components/menu-item/menu-item.components.jsx
+++ src/Components/menu-item/menu-item.components.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './menu-items.styles.scss'
 import { withRouter } from 'react-router-dom';
 
+
 const MenuItem = ({title, imageUrl, size, history,linkUrl, match}) => {
     return(
         <div  className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
