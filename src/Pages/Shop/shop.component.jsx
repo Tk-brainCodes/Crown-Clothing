1ed30a7e7dfd4fe69dbd99fe08cc70839298d8b6
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-03-03 13:30:56 +0100

    added firebase collection doc

--- src/Pages/Shop/shop.component.jsx
+++ src/Pages/Shop/shop.component.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import {useEffect} from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 import CollectionOverview from '../../Components/collection-overview/collection.overview.component';
 import CollectionPage from '../collection/collection.component';
+import { firestore, convertCollectionSnapshotToMap } from '../../firebase/firebase.utils';
+import { updateCollections } from '../../redux/shop/shop.action';
 
-const Shop = ({match}) => {
+const Shop = ({match, updateCollections}) => {
+    let unsubscribeFromSnashot = null;
+    useEffect(() => {
+       const collectionRef = firestore.collection('collections');
+
+      unsubscribeFromSnashot = collectionRef.onSnapshot(async snapshop => {
+        const collectionMap = convertCollectionSnapshotToMap(snapshop);
+        updateCollections(collectionMap);
+       })
+    }, [unsubscribeFromSnashot]);
+
+    
     return (
         <div className="shop-page">
             <Route exact path={`${match.path}`} component={CollectionOverview}/>
@@ -11,4 +25,8 @@ const Shop = ({match}) => {
         </div>)
 }
 
-export default Shop;
+const mapDispatchToProps = dispatch => ({
+    updateCollections: collectionMap => dispatch(updateCollections(collectionMap))
+})
+
+export default connect(null,mapDispatchToProps)(Shop);
