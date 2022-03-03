3faed8527cfdd642881e90229cd71167425558bc
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-03-03 13:26:53 +0100

    added logout

--- src/Components/header/header.component.jsx
+++ src/Components/header/header.component.jsx
@@ -18,7 +18,7 @@ const Header = ({currentUser, hidden}) => {
                 <OptionLink to="/shop">SHOP</OptionLink>
                  <OptionLink to="/contact">CONTACT</OptionLink>
                  {
-                     currentUser ?
+                     currentUser === true ?
                      (
                      <OptionDiv onClick={() => auth.signOut()}>
                         SIGN OUT
