7481d18b78e2e1505f831af61e27e01a3b148601
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-03-04 23:25:28 +0100

    modified

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import {shallow, mount, render} from 'enzyme';
-import React from 'react';
-import App from './App';
-
-it("expects to render App Component", (done) => {
-    expect.assertions(1);
-    expect(shallow(<App/>).toEqual(1));
-    done();
-})
