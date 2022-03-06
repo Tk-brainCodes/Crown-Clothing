0aee3abeac4d08c7fa2dfda808d425ef52bdb1b2
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-03-06 20:54:37 +0100

    redux action test

---
+++ src/redux/actions.test.js
@@ -0,0 +1,18 @@
+import { setCurrentUser } from "./user/user.actions";
+import { updateCollections } from "./shop/shop.action";
+import { toggleCartHidden, addItem, clearItem, removeItem } from "./cart/cart.action";
+import  {UserActionTypes}  from './user/user.types';
+import {ShopActionTypes} from './shop/shop.types';
+import {CartActionTypes} from './cart/cart.action.types';
+
+
+describe("make sure current user action works", () => {
+   it("should create a current user action ", () => {
+     const user = "thankgod"
+     const expectedAction = {
+       type: UserActionTypes.SET_CURRENT_USER,
+       payload: user
+     }
+     expect(setCurrentUser(user)).toEqual(expectedAction)
+   })
+})
