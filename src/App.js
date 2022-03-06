76f63c7baa8c553213aab0e39c4452a2b1546cb7
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-03-06 13:27:11 +0100

    imported main component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import {useEffect} from 'react';
 import {connect} from 'react-redux';
 import './App.css';
-import Homepage from '../src/Pages/Homepage/Homepage.component';
-import Shop from './Pages/Shop/shop.component';
-import Checkout from './Pages/checkout/checkout.components';
-import Header from './Components/header/header.component';
-import { Switch, Route, Redirect} from 'react-router-dom'
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 import {setCurrentUser} from './redux/user/user.actions';
 import { createStructuredSelector } from 'reselect';
-import SignInAndSignUpPage from './Pages/sign-in-and-sign-up/sign-in-and-sign-up.components';
 import { selectCurrentUser } from './redux/user/user.selector';
+import MainPage from './Pages/MainPage/MainPage.component';
 
 function App({currentUser}) {
   useEffect(() => {
-   auth.onAuthStateChanged(async userAuth => {
+  auth.onAuthStateChanged(async userAuth => {
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth)
         userRef.onSnapshot(snapShot => {
@@ -31,13 +26,7 @@ function App({currentUser}) {
   
   return (
     <div className="App">
-      <Header/>
-      <Switch>
-        <Route exact path="/" component={Homepage}/>
-        <Route path="/Shop" component={Shop} />
-        <Route exact path='/checkout' component={Checkout} />
-        <Route exact path="/signin" render={() => currentUser === true ? <Redirect to="/" /> : <SignInAndSignUpPage />} /> 
-      </Switch>
+      <MainPage currentUser={currentUser}/>
     </div>
   );
 }
