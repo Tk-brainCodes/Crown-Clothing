62bdc44f1c635eedc014d1d4de8595f7a2715b23
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-03-07 19:00:42 +0100

    updated shop actions

--- src/redux/shop/shop.action.js
+++ src/redux/shop/shop.action.js
@@ -1,7 +1,29 @@
 import ShopActionTypes from "./shop.types";
+import { firestore, convertCollectionSnapshotToMap } from "../../firebase/firebase.utils";
 
 
-export const updateCollections = collectionsMap => ({
-    type: ShopActionTypes.UPDATE_COLLECTIONS,
-    payload: collectionsMap
+export const fetchCollectionsStart = () => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_START,
 });
+
+export const fetchCollectionsSuccess = collectionMap => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+    payload: collectionMap
+})
+
+export const fetchCollectionsFailure = errorMessage => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+    payload: errorMessage
+})
+
+//redux thunk at work
+export const fetchCollectionsStartAsync = () => {
+    return dispatch => {
+        const collectionRef = firestore.collection('collections'); 
+        dispatch(fetchCollectionsStart());
+        collectionRef.get().then(snapshop => {
+          const collectionMap = convertCollectionSnapshotToMap(snapshop);
+         dispatch(fetchCollectionsSuccess(collectionMap));
+         }).catch(error => dispatch(fetchCollectionsFailure(error.message)))
+    }
+}
