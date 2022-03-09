b8a32f5e8b860d0cda10b10078adee3059770ce8
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-03-09 23:43:59 +0100

    removed redux thunk function

--- src/redux/shop/shop.action.js
+++ src/redux/shop/shop.action.js
@@ -1,5 +1,4 @@
 import ShopActionTypes from "./shop.types";
-import { firestore, convertCollectionSnapshotToMap } from "../../firebase/firebase.utils";
 
 
 export const fetchCollectionsStart = () => ({
@@ -16,14 +15,3 @@ export const fetchCollectionsFailure = errorMessage => ({
     payload: errorMessage
 })
 
-//redux thunk at work
-export const fetchCollectionsStartAsync = () => {
-    return dispatch => {
-        const collectionRef = firestore.collection('collections'); 
-        dispatch(fetchCollectionsStart());
-        collectionRef.get().then(snapshop => {
-          const collectionMap = convertCollectionSnapshotToMap(snapshop);
-         dispatch(fetchCollectionsSuccess(collectionMap));
-         }).catch(error => dispatch(fetchCollectionsFailure(error.message)))
-    }
-}
