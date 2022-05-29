1436b889de93d96aa7a1c27c451e16a90336da7f
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-05-29 11:51:23 +0100

    Feature: Add path for stripe payment from backend server
    installed axios for fetch request

--- src/Components/stripe-button/stripe-button.component.tsx
+++ src/Components/stripe-button/stripe-button.component.tsx
@@ -1,39 +1,55 @@
-import React, {Fragment} from 'react'
-import { ToastContainer, toast } from 'react-toastify'
-import StripeCheckout from 'react-stripe-checkout'
-import 'react-toastify/dist/ReactToastify.css'
+import React, { Fragment } from "react";
+import StripeCheckout from "react-stripe-checkout";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 
 interface Price {
-    price: number
+  price: number;
 }
 
-const StripeCheckoutButton = ({price}: Price) => {
-    const priceForStripe = price * 100;
-    const publishableKey: string = 'pk_test_51IGZ6EJWDBULoxk2muGXqz9RpyZtR6zmQdyb9QN4CJZNlXAFt6yaIponKddgxgnDveYTY3ISb2V1UR177txFJj1i00K0jBKRWQ';
+const StripeCheckoutButton = ({ price }: Price) => {
+  const priceForStripe = price * 100;
+  const publishableKey: string =
+    "pk_test_51IGZ6EJWDBULoxk2muGXqz9RpyZtR6zmQdyb9QN4CJZNlXAFt6yaIponKddgxgnDveYTY3ISb2V1UR177txFJj1i00K0jBKRWQ";
 
-   const onToken = (token: any) : void => {
-        console.log(token)
-        toast('Payment Successful!')
-        // alert("payment successful")
-    }
-    return (
-       <Fragment>
+  const onToken = (token: any): void => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("payment successful");
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log("There was an error", error);
+        alert(
+          "there was an issue with your payment. Please use the required credit card"
+        );
+      });
+  };
+
+  return (
+    <Fragment>
       <StripeCheckout
-         label='Pay Now'
-         name='CROWN CLOTHING Org.'
-         amount={priceForStripe}
-         currency='USD'
-         billingAddress
-         shippingAddress
-         image = 'https://svgshare.com/i/CUz.svg'
-         description={`Your total is $${price}`}
-         panelLabel='Pay Now'
-         token={onToken}
-         stripeKey={publishableKey}
-       />
-       <ToastContainer/>
-       </Fragment>
-    )
-}
+        label="Pay Now"
+        name="CROWN CLOTHING Org."
+        amount={priceForStripe}
+        currency="USD"
+        billingAddress
+        shippingAddress
+        image="https://svgshare.com/i/CUz.svg"
+        description={`Your total is $${price}`}
+        panelLabel="Pay Now"
+        token={onToken}
+        stripeKey={publishableKey}
+      />
+    </Fragment>
+  );
+};
 
 export default StripeCheckoutButton;
