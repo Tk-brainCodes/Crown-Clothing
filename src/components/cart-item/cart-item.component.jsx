9ab4e1ae26d89b588b3ed349a3a731290e99875f
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2022-06-06 11:47:09 +0100

    Feature: Updated favicon Change site favicon manifest.json

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import './cart-item.styles.scss';
+import React from "react";
+import "./cart-item.styles.scss";
 
-
-const CartItem = ({item: {imageUrl, name, price, quantity}}) => {
-    return (
-        <div className='cart-item'>
-           <img src={imageUrl} alt="item" />
-            <div className="item-details">
-                <span className="name">{name}</span>
-                <span className="price">{quantity} x ${price}</span>
-            </div>
-        </div>
-    )
-}
-export default CartItem;
+const CartItem = ({ item: { imageUrl, name, price, quantity } }) => {
+  return (
+    <div className="cart-item">
+      <img src={imageUrl} alt="item" />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">
+          {quantity} x ${price}
+        </span>
+      </div>
+    </div>
+  );
+};
+export default React.memo(CartItem);
--- src/pages/Homepage/Homepage.component.tsx
+++ src/pages/Homepage/Homepage.component.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { Profiler } from "react";
 import Directory from "../../components/directory/directory.component";
 import { HomepageContainer } from "./Homepage.styles";
 const Homepage = () => {
   return (
     <HomepageContainer>
-      <Directory />
+      <Profiler
+        id="Directory"
+        onRender={(id, phase, actualDuration) => {
+          console.log({
+            id,
+            phase,
+            actualDuration,
+          });
+        }}
+      >
+        <Directory />
+      </Profiler>
     </HomepageContainer>
   );
 };
